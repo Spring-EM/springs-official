64f1acc5419a26c706a16f60791cae2f76c1ce2c
Hill Onyechekwa <hillonyechekwa@gmail.com>
2022-11-25 05:39:16 +0100

    added counter scroll animation feature

--- js/scroll.js
+++ js/scroll.js
@@ -1,29 +1,63 @@
-'use strict';
+// pseudo code
+//add data-target attribs to number counter elements
+//set the values to the target numbers to scroll count into.
+//give each data target a class of counter
+//select counter container
+//select all the elements with class counter
+//set a variable speed to 200
+//map through each array item of selected counter classes
+//use item and index as parameters
+//create an updatecounter function within callback
+//select the data-target attribute value (dataset.target) and set to target number variable
+//print taget to console.
+//select the inner text in the counter element
+//use the speed variable to calculate the increment per count
+//write an if statement with a conditional that checks the initialnumber against the target number
+//if the conditional returns true, update the inner text of the counter to the sum of the initial number and the increment per count
+//add a setTimeOut with a duration of  40s and a callback of the updateCounter function
+//use the math function to round up the new inner text
+//add the addition operator to the start of the vlaues for the target number and the initial number
 
 
-function scrollTrigger(selector, options = {}) {
-    let els = document.querySelectorAll(selector)
+// gsap.registerPlugin(ScrollTrigger)
 
-    els = Array.from(els)
 
-    els.forEach(el => {
-        addObserver(el, options)
-    })
-}
+let counterContainer = document.getElementById("section-counter")
 
-function addObserver(el, options) {
-    let observer = new IntersectionObserver((entries, observer) => {
-        entries.forEach(entry => {
-            if(entry.isIntersecting) {
-                entry.target.classList.add('scrolled')
+let counters = document.querySelectorAll(".counter-item .counter")
 
-                observer.unobserve(entry.target)
+let speed = 100
+
+
+
+
+//Counter Scroll Animation
+
+let counterObserver = new IntersectionObserver((entries, observer) => {
+    let  [entry] = entries
+    if (entry.isIntersecting) {
+        counters.forEach((counter, index) => {
+          function updateCounter() {
+            let targetNumber = +counter.dataset.target;
+            let initialNumber = +counter.innerHTML;
+            let incrPerCount = targetNumber / speed;
+            if (initialNumber < targetNumber) {
+              counter.innerHTML = Math.ceil(initialNumber + incrPerCount);
+              setTimeout(updateCounter, 40);
             }
-        })
-    }, options)
-    observer.observe(el)
-}
+          }
 
-scrollTrigger('.scroll-counter', {
-    rootMargin: '-200px'
+          updateCounter();
+        });
+    }
+ }, {
+    root: null,
+    threshold: 0.4
 })
+
+counterObserver.observe(counterContainer)
+
+// gsap.to(counterContainer, {
+//     scrollTrigger: counterContainer,
+//     x: 120
+// })
