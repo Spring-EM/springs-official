929ffbbfb1d831ef793fc0b66a3fa4680e05803b
symply temmy <temiloluwasharon710@gmail.com>
2023-02-04 20:40:27 +0100

    added close button in hamburger menu

--- js/main.js
+++ js/main.js
@@ -4,4 +4,5 @@ var hamburger = document.querySelector(".hamburger")
 hamburger.addEventListener("click", function() {
     var navBar = document.querySelector(".nav-items")
     navBar.classList.toggle("active")
+    hamburger.classList.toggle("nav-clicked")
 })
